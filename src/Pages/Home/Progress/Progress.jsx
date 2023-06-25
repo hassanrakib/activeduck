7f478bf869dacfca81c1f9e91488172b13eeb600
rakibhassan3 <mailrakibhassan@gmail.com>
2023-06-25 10:26:31 +0600

    showing targetTime and completedTime and progress bar moving

--- src/Pages/Home/Progress/Progress.jsx
+++ src/Pages/Home/Progress/Progress.jsx
@@ -1,37 +1,73 @@
-import { millisecondsToHours, millisecondsToMinutes } from "date-fns";
+import { formatDuration, millisecondsToHours, millisecondsToMinutes } from "date-fns";
 import styles from "./Progress.module.css";
 import withTaskProgressCalculation from "../../../HOC/withTaskProgressCalculation";
-const Progress = ({ completedTimeInMilliseconds, isTaskActive }) => {
+const Progress = ({
+  completedTimeInMilliseconds,
+  isTaskActive,
+  levels,
+  currentLevel,
+}) => {
+  // destructure to get every level
+  const { level_1, level_2, level_3 } = levels;
 
   // converts the time in milliseconds to {hours: 3, minutes: 30} type object
-  const converToDurationObject = (timeInMilliseconds) => {
-    return {
+  // then return a human readable string that uses the object
+  const convertToHumanReadableTime = (timeInMilliseconds) => {
+    const duration = {
       hours: millisecondsToHours(timeInMilliseconds),
       // removing the number of milliseconds for hours by using remainder operator
-      // then the remaining milliseconds are converted to minutes 
+      // then the remaining milliseconds are converted to minutes
       minutes: millisecondsToMinutes(timeInMilliseconds % (1000 * 60 * 60)),
     };
+    
+    return formatDuration(duration);
   };
 
-  // convert the completedTimeInMilliseconds to a duration object like {hours: 2, minutes: 20};
-  const completedTime = converToDurationObject(completedTimeInMilliseconds);
+  // convert the completedTimeInMilliseconds to a human readable string that we show in the ui
+  const completedTime = convertToHumanReadableTime(completedTimeInMilliseconds);
 
-  console.log(completedTime);
+  // define targetTime and targetTimeInMilliseconds based on currentLevel
+  // targetTime is human readable that we show in the ui
+  let targetTime;
+  // targetTimeInMilliseconds for progress tag's max attribute
+  let targetTimeInMilliseconds;
+
+  switch (currentLevel) {
+    case "Level - 1":
+      // convertToHumanReadable function takes current level's time in milliseconds
+      // outputs a final human readable string to show in the ui
+      targetTime = convertToHumanReadableTime(level_1);
+      // targetTimeInMilliseconds holds the current level's time in milliseconds
+      targetTimeInMilliseconds = level_1;
+      break;
+    case "Level - 2":
+      targetTime = convertToHumanReadableTime(level_2);
+      targetTimeInMilliseconds = level_2;
+      break;
+    case "Level - 3":
+      targetTime = convertToHumanReadableTime(level_3);
+      targetTimeInMilliseconds = level_3;
+  }
+
+  // calculate tooltip width by calculating
+  // how much percentage is completedTimeInMilliseconds of the targetTimeInMilliseconds 
+  const tooltipWidth = (completedTimeInMilliseconds * 100) / targetTimeInMilliseconds;
+
 
   return (
     <div className={styles.progressContainer}>
       {/* move the tooltip as progress value increases by changing width */}
       {/* to understand more see the css styles */}
-      <div className={styles.progressTooltip} style={{ width: "20%" }}>
+      <div className={styles.progressTooltip} style={{ width: `${tooltipWidth}%` }}>
         <span className={styles.progressTooltipText}>
-          {completedTime.hours && `${completedTime.hours}h `}
-          {`${completedTime.minutes}m`} of 2h 30m completed
+          {completedTime} of {targetTime} completed
         </span>
       </div>
       <progress
         className={isTaskActive ? `${styles.animate}` : ""}
-        max="100"
-        value="20"
+        // max is the number of the  and value is the completedTime
+        max={targetTimeInMilliseconds}
+        value={completedTimeInMilliseconds}
       ></progress>
     </div>
   );
